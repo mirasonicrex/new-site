0151adf5f3abca747122fa34f9b52e978cb27197
Mira Heckmann <miraheckmann@gmail.com>
2024-04-08 17:31:20 -0400

    recent project

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,21 +1,50 @@
-import styles from './about.module.css';
+import styles from "./about.module.css";
 
 const About = () => {
   return (
     <div className={styles.container}>
-      <img src="portfolio/me.jpg" alt="Profile" className={`${styles.profileImage} ${styles.fadeInUp}`} width="30%" height="auto" />
+      <img
+        src="portfolio/me.jpg"
+        alt="Profile"
+        className={`${styles.profileImage} ${styles.fadeInUp}`}
+        width="30%"
+        height="auto"
+      />
       <div className={styles.about}>
         <p className={`${styles.aboutText} ${styles.fadeInUp}`}>
-          <b>Software Engineer</b> with experience in <b>Full Stack Development</b> and <b>Testing</b>
+          <b>Software Engineer</b> with experience in{" "}
+          <b>Full Stack Development</b> and <b>Testing</b>
         </p>
-        </div>
-        <div className={styles.links}>
-          <a href="https://www.linkedin.com/in/miraheckmann/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
-          <a href="https://github.com/mirasonicrex" target="_blank" rel="noopener noreferrer">GitHub</a>
-        </div>
-  
+      </div>
+      <div className={styles.links}>
+        <a
+          href="https://www.linkedin.com/in/miraheckmann/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          LinkedIn
+        </a>
+        <a
+          href="https://github.com/mirasonicrex"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          GitHub
+        </a>
+      </div>
+      <div className={styles.skills}>
+        <div className={styles.technologies}>React.js</div>
+        <div className={styles.technologies}>Three.js</div>
+        <div className={styles.technologies}>Python</div>
+        <div className={styles.technologies}>CSS</div>
+        <div className={styles.technologies}>TypeScript</div>
+        <div className={styles.technologies}>Node.js</div>
+        <div className={styles.technologies}>MongoDB</div>
+        <div className={styles.technologies}>DynamoDB</div>
+      </div>
     </div>
+  
   );
-}
+};
 
 export default About;
--- src/components/ThreeProject.tsx
+++ src/components/ThreeProject.tsx
@@ -1,7 +1,27 @@
 import styles from './three.module.css'
 const ThreeProject: React.FC = () => {
 return (
-    <div className={styles.container}>A Porject with  three.js and react</div>
+    <div className={styles.container}>
+        <h2>Recent Projects</h2>
+        <h3>Three.js Website - April 2024</h3>
+         <img width="650" height="auto" src="imgs/aprilScreenshot.png"></img>
+         <div>
+         <div className={styles.technologies}>Three.js</div>
+        <div className={styles.technologies}>React</div>
+        <div className={styles.technologies}>Typescript</div>
+        <div className={styles.technologies}>  CSS</div>
+         </div>
+    
+        <p>An experimental site created with Three.js and React that guides the user through the progress of my indie game ./ThisIsTheGame</p>
+        <a 
+        className={styles.link}
+              href="https://github.com/mirasonicrex/three-js-website"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              GitHub Repo
+            </a>
+    </div>
 )
 }
 
